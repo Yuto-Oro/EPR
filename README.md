c0656998512d49419ae46bcdce46e36f7f9bf23f
Orlando Ortega <monkeydoro@gmail.com>
2020-06-26 20:18:32 -0500

    Día #2
    FT Cards Finished
    
    <CHANGELOG>
    Se arreglo el modulo de crearDeck.
    Se agrego el modulo contains.
    Se agrego el modulo deal.
    Se agrego el modulo para guardar y cargar archivos en el sistema.
    Se uso pattern matching para hacer más eficiente el inicio del "juego".
    </CHANGELOG>

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # EPR
 
 *Repository for the EP Course*
+
+**Para iniciar, iex -S mix**
+
+**Para instalar dependencias, en mix.exs -> deps**
+*Una vez agregada la dependencia, lo corremos con:**mix deps.get***
+
--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,9 +1,45 @@
 defmodule Cards do
  def createDeck do
- ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "four", "five"]
+    suits = ["spades", "clubs", "hearts", "diamons"]
+    
+    for suit <- suits,value <- values do
+        "#{value} of #{suit}"
+    end
+    
  end
 
  def shuffle(deck) do
-  Enum.shuffle(deck)
+    Enum.shuffle(deck)
+ end
+
+ def contains?(deck, card) do
+    Enum.member?(deck, card)
+ end
+
+ def deal(deck, handSize) do
+ #Nos regresa una tupla que en el index 0 tendrá lo que pedimos.
+    Enum.split(deck, handSize)
+ end
+
+#Modulo para guardar el archivo en el sistema.
+ def save(deck, fileName) do
+     binary = :erlang.term_to_binary(deck)
+     File.write(fileName, binary)
+ end
+
+#Modulo para cargar datos desde el disco
+ def load(fileName) do
+    case File.read(fileName) do #Pattern Matching con la tupla en el case
+    #Comparamos el resultado de File.read() y lo asignamos dependiendo si es error o no a la variable.
+        {:ok, binary} -> :erlang.binary_to_term binary
+        {:error, _reason} -> "El archivo no existe" # _ Se usa para ignorar una variable
+    end
+ end
+#Modulo que concatena los otros módulos con ' | '
+ def createHand(handSize) do
+    Cards.createDeck #Toma el return value y lo va pasando
+    |> Cards.shuffle
+    |> Cards.deal(handSize)
  end
 end
